a89697939564c11c183fae4a9ba5a7be555de67f
maxandbe1 <maxandbe1@proton.me>
2026-05-19 21:36:49 -0400

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,15 @@
+const AppState = {
+  currentSurface: 'games',
+  currentGameId: null,
+  monetization: {
+    lastEvent: null,
+    totalEvents: 0
+  },
+  system: {
+    status: 'READY',
+    lastError: null
+  }
+};
+
+export { AppState };
+
